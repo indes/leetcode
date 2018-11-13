838f14275baf9f26a33c2415e25d33c29a6d82db
indes <wanglicsly@gmail.com>
2018-11-13 18:43:21 +0800

    pass find-minimum-in-rotated-sorted-array

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 
-#include "search-in-rotated-sorted-array/Solution.h"
+#include "find-minimum-in-rotated-sorted-array/Solution.h"
 
 using namespace std;
 
 int main() {
     Solution s;
-    vector<int> v1 = {4, 5, 6, 7, 0, 1, 2}, v2 = {1, 2, 3, 5, 6};
+    vector<int> v1 = {4, 5, 6, 7, 0, 1, 2}, v2 = {1,2,3};
 //    s.merge(v1, 1, v2, 5);
 //    auto a = s.twoSum(v2, 7);
-    int a = s.search(v1, 0);
+    int a = s.findMin(v2);
     return 0;
 }
 

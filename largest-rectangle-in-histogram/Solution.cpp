ba4a5816459d420757644ed02721e15ca63b91c5
indes <wanglicsly@gmail.com>
2018-11-15 21:33:54 +0800

    pass largest-rectangle-in-histogram

---
+++ largest-rectangle-in-histogram/Solution.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Li on 2018/11/15.
+//
+
+#include "Solution.h"
+
+int Solution::largestRectangleArea(vector<int> &heights) {
+
+
+}
---
+++ largest-rectangle-in-histogram/Solution.h
@@ -0,0 +1,16 @@
+//
+// Created by Li on 2018/11/15.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights);
+};
+
+
+#endif //LEETCODE_SOLUTION_H

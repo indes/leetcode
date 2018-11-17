76d6c990a16370bf1087214224ccf8e51f132775
indes <wanglicsly@gmail.com>
2018-11-17 15:32:02 +0800

    pass linked-list-cycle

--- linked-list-cycle/Solution.cpp
+++ linked-list-cycle/Solution.cpp
@@ -5,12 +5,14 @@
 #include "Solution.h"
 
 bool Solution::hasCycle(ListNode *head) {
-    if(head == nullptr) return false;
-    ListNode *p2 = head;
 
-    while (p2->next != nullptr) {
+    if (head == NULL or head->next == NULL) return false;
+    ListNode *p1 = head, *p2 = head;
+
+    while (p1->next != NULL and p1->next->next != NULL) {
+        p1 = p1->next->next;
         p2 = p2->next;
-        if(p2 == head) return true;
+        if (p2 == p1) return true;
     }
     return false;
 }

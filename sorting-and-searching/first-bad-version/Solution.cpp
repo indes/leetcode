c08c839c27445f74670a4faad8b3bdcec460c726
indes <wanglicsly@gmail.com>
2018-11-27 11:15:18 +0800

    :sparkles: Pass first-bad-version

---
+++ sorting-and-searching/first-bad-version/Solution.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Li on 2018/11/27.
+//
+
+#include "Solution.h"
+
+int Solution::firstBadVersion(int n) {
+    long min = 1;
+    long max = n;
+    if (min == max) return min;
+    long mid = (min + max) >> 1;
+
+    while (max > min) {
+        if (isBadVersion(mid)) max = mid ;
+        else min = mid + 1;
+        mid = (min + max) >> 1;
+    }
+
+    return min;
+}
---
+++ sorting-and-searching/first-bad-version/Solution.h
@@ -0,0 +1,15 @@
+//
+// Created by Li on 2018/11/27.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+bool isBadVersion(int version);
+class Solution {
+public:
+    int firstBadVersion(int n);
+};
+
+
+#endif //LEETCODE_SOLUTION_H

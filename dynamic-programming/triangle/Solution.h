9168d264b5f28b69471d17b744233413f82a2de4
indes <wanglicsly@gmail.com>
2018-11-28 22:23:06 +0800

    :sparkles: Pass triangle

---
+++ dynamic-programming/triangle/Solution.h
@@ -0,0 +1,18 @@
+//
+// Created by Li on 2018/11/28.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>> &triangle);
+};
+
+
+#endif //LEETCODE_SOLUTION_H

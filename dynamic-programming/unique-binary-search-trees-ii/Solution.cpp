0f2d31ac1a442ecd9a61090e92623c771da81fd9
indes <wanglicsly@gmail.com>
2018-11-29 15:33:52 +0800

    :hankey: War unique-binary-search-trees-ii

---
+++ dynamic-programming/unique-binary-search-trees-ii/Solution.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Li on 2018/11/29.
+//
+
+#include "Solution.h"
+
+vector<TreeNode *> Solution::generateTrees(int n) {
+
+
+    return vector<TreeNode *>();
+}
---
+++ dynamic-programming/unique-binary-search-trees-ii/Solution.h
@@ -0,0 +1,20 @@
+//
+// Created by Li on 2018/11/29.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+
+#include "../../tree/TreeNode.h";
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<TreeNode *> generateTrees(int n);
+};
+
+
+#endif //LEETCODE_SOLUTION_H

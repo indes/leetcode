908e291320eaa84861640a6e541ec1de111f5e1e
indes <wanglicsly@gmail.com>
2018-12-01 10:41:44 +0800

    :hankey: War maximum-product-subarray

---
+++ dynamic-programming/maximum-product-subarray/Solution.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Li on 2018/11/30.
+//
+
+#include "Solution.h"
+
+int Solution::maxProduct(vector<int> &nums) {
+    
+    return 0;
+}
---
+++ dynamic-programming/maximum-product-subarray/Solution.h
@@ -0,0 +1,19 @@
+//
+// Created by Li on 2018/11/30.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums);
+};
+
+
+#endif //LEETCODE_SOLUTION_H

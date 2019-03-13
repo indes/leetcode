a3db7289d2a7fbe7a6ad1a32736a20aaa082a4be
indes <wl@live.it>
2019-03-13 09:55:41 +0800

    :sparkles: Pass longest-increasing-subsequence with O(n^2)

---
+++ dynamic-programming/longest-increasing-subsequence/Solution.h
@@ -0,0 +1,39 @@
+//
+// Created by Li on 2019/3/13.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <vector>
+#include <algorithm>
+
+using std::max;
+using std::vector;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        // O(n^2)
+
+        if (nums.empty()) {
+            return 0;
+        }
+        vector<int> v(nums.size(), 1);
+        int max_length = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    v[i] = max(v[i], v[j] + 1);
+                }
+
+            }
+            max_length = max(max_length, v[i]);
+
+        }
+        return max_length;
+    }
+};
+
+
+#endif //LEETCODE_SOLUTION_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 
-#include "backtracking/word-search/Solution.h"
-
-using namespace std;
+#include "dynamic-programming/longest-increasing-subsequence/Solution.h"
 
 using namespace std;
 
 int main() {
     Solution s;
-//    vector<string> v = {3, 3, 0, 3};
-    vector<vector<char>> v = {
-            {'a', 'b'}
-//            {'S', 'F', 'C', 'S'},
-//            {'A', 'D', 'E', 'E'}
-    };
-    auto v2 = s.exist(v, "ba");
+    vector<int> v = {0};
+
+    auto res = s.lengthOfLIS(v);
 //    cout << v2 << endl;
     return 0;
 }

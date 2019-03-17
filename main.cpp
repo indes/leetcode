484c28972c2a7857c5b40dd8466615b60d5149cf
indes <wl@live.it>
2019-03-17 09:27:52 +0800

    :sparkles: Pass find-peak-element

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 
-#include "backtracking/palindrome-partitioning/Solution.h"
+#include "array/find-peak-element/Solution.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
     Solution s;
-
-    auto res = s.partition("aab");
+    vector<int> v = {1, 2, 3, 1};
+    auto res = s.findPeakElement(v);
+//    auto res = s.partition("aab");
 //    cout << v2 << endl;
+    cout << res << endl;
     return 0;
 }
 

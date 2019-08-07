0d7f6b8be39366b4649aff262e3eab9dd47fb1a4
indes <wl@live.it>
2019-08-07 21:24:39 +0800

    :recycle: Review 45.jump-game-ii

---
+++ leetcode/45-jump-game-ii/Solution.h
@@ -0,0 +1,33 @@
+//
+// Created by Li on 2018/12/3.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int> &nums) {
+        int end; // 上一次停靠点能跳跃的边界
+        int max_place = 0; // 当前能跳的最远距离
+        int count = 0;
+
+        for (auto i = 0; i < nums.size() -1 ; ++i) {
+            max_place = std::max(max_place, nums[i] + i); // 保留最远距离
+
+            if (i >= end) {
+                ++count;
+                end = max_place;
+            }
+        }
+        return count;
+    }
+};
+
+
+#endif //LEETCODE_SOLUTION_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 
-#include "./leetcode/46-permutations/Solution.h"
+#include "./leetcode/45-jump-game-ii/Solution.h"
 
 #include <iostream>
 
@@ -8,8 +8,8 @@ using namespace std;
 int main() {
     Solution s;
 
-    vector<int> v = {1, 2, 3};
-    auto p = s.permute_stl(v);
+    vector<int> v = {2, 3, 0, 1, 4};
+    auto p = s.jump(v);
 
     return 0;
 }

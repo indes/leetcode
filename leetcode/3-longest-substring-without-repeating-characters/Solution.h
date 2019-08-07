ea32fc9e7fa2347170f12a5d38e5c91746524747
indes <wl@live.it>
2019-08-07 21:58:13 +0800

    :recycle: Review 3.longest-substring-without-repeating-characters

--- leetcode/3-longest-substring-without-repeating-characters/Solution.h
+++ leetcode/3-longest-substring-without-repeating-characters/Solution.h
@@ -25,7 +25,6 @@ public:
                 } else {
                     len++;
                 }
-//                len = min(i - m[s[i]], len);
             } else {
                 len++;
             }
